5df7ec4765c56fa89cb708d1490d0e3529353599
Tomona Sako <sakotomona@gmail.com>
2020-05-18 09:28:45 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Modal from "react-modal";
+ 
+Modal.setAppElement('#root')
 
 function App() {
+  const style = {
+    width: "50%",
+    margin: "0 auto",
+    marginTop: 150
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={style}>
+        <Modal></Modal>
+      </div>
     </div>
   );
 }
-
+ 
 export default App;
+ 
---
+++ src/components/atoms/Td.js
@@ -0,0 +1,11 @@
+import { createStore } from 'redux'
+import React from 'react';
+
+
+function changeTd(td) {
+    return {type: '', payload: td}
+}
+
+const Td() {
+
+}
