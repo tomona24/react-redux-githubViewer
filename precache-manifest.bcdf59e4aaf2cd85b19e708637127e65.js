1a6fe28262f8aa06aef184f853f54a08752859b4
Tomona Sako <sakotomona@gmail.com>
2020-06-15 10:12:23 -0700

    Updates

---
+++ precache-manifest.bcdf59e4aaf2cd85b19e708637127e65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e6d23b84cd7d787a571ecf3c5fa912",
+    "url": "/react-redux-githubViewer/index.html"
+  },
+  {
+    "revision": "65d61cd579efe8865d8a",
+    "url": "/react-redux-githubViewer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7c7c2b1764b7ddb4955f",
+    "url": "/react-redux-githubViewer/static/js/2.398bb5cb.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/react-redux-githubViewer/static/js/2.398bb5cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65d61cd579efe8865d8a",
+    "url": "/react-redux-githubViewer/static/js/main.f7419993.chunk.js"
+  },
+  {
+    "revision": "728ce1a3389f4647c695",
+    "url": "/react-redux-githubViewer/static/js/runtime-main.3ced559a.js"
+  }
+]);
